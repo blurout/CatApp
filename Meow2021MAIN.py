1ea335c6139059a5b4b091d41c8db0073af7621a
sharpaxe <sharpaxiee@gmail.com>
2021-09-16 17:55:52 -0700

    Added image display and added functions

--- Meow2021MAIN.py
+++ Meow2021MAIN.py
@@ -1,9 +1,24 @@
 import requests
-f = open('key.config','r')
-key = f.read()
-f.close()
+from PIL import Image
+from io import BytesIO
 
-for i in range(1,100):
-    r = requests.get('https://api.thecatapi.com/v1/images/search', params={'x-api-key': key})
+
+def get_cat_image():
+    r = requests.get('https://api.thecatapi.com/v1/images/search', params={'x-api-key': get_api_key()})
     data = r.json()
-    print(data[0]['url'])
+    url = data[0]['url']
+    response = requests.get(url)
+    return BytesIO(response.content)
+
+
+def get_api_key():
+    f = open('key.config', 'r')
+    key = f.read()
+    f.close()
+    return key
+
+
+if __name__ == '__main__':
+    for i in range(0, 30):
+        img = Image.open(get_cat_image())
+        img.show()
